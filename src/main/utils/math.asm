2cde0fe3f6fb62501e679cb8da4e7fef7d9019ee
Alejandro Tomás Martínez <57414454+itsAler@users.noreply.github.com>
2026-01-27 14:11:17 +0100

    cambios

--- src/main/utils/math.asm
+++ src/main/utils/math.asm
@@ -27,7 +27,7 @@ rand::
   ld b, a
   ret
 
-; Almacena en A el entero real de 1 Byte desde un entero escalado de 2 Bytes en BC.
+; Almacena en A el entero real de 1 Byte desde un entero escalado de 2 Bytes almacenado en BC.
 deEscaleBCtoA::
   srl c
   rr b
