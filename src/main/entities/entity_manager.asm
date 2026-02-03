34bb64e84e98ccf314dc2250fb00e84dda19a745
Alejandro Tomás Martínez <57414454+itsAler@users.noreply.github.com>
2026-02-03 10:29:11 +0100

    implementado logic updater en entity manager y refactoring de código

--- src/main/entities/entity_manager.asm
+++ src/main/entities/entity_manager.asm
@@ -4,8 +4,8 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ENTITY ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;
-;   1B FLAGS [7: VALID] [6-5: ENT_TYPE] [4: INITIALIZED] [3-0: EFECTS]
-;       VALID:      La entrada en la lista de entidades está siendo usada.
+;   1B FLAGS [7: STATUS] [6-3: ENT_TYPE] [2-0: UNUSED]
+;       STATUS:     Estado actual de la entrada (inválida, válida)
 ;       ENT_TYPE:   Tipo de la entidad (jugador, enemigo...)
 ;       INITIALIZED: La entidad acaba de crearse o ha sido colocada ya en el mundo.
 ;       EFECTS:      Efecto aplicado sobre la entidad (daño, fuego, hielo...)
@@ -15,27 +15,23 @@
 ;   1B MOMENTUM_INC (BIT 7-4) [0, 127] | MOMENTUM_DEC (BIT 3-0) [0, 127]
 ;   1B HEALTH [0, 255]
 ;   1B DAMAGE [0, 255]
-;   2B ENTITY_METADATA
 ;   2B SCALED_X
 ;   2B SCALED_Y
 ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-DEF ENT_SZ  EQU 13
+DEF ENT_SZ  EQU 11
 DEF ENT_LIST_MAX EQU 4 ; Límite actual de 256 entidades
-
-DEF ENT_FLAG_VALID          EQU %10000000
-DEF ENT_VALID               EQU %10000000
-DEF ENT_INVALID             EQU %00000000
-DEF ENT_FLAG_TYPE           EQU %01100000
+; Atributos y máscaras
+DEF ENT_FLAG_STATUS         EQU %10000000
+DEF ENT_STATUS_VALID        EQU %10000000
+DEF ENT_STATUS_INVALID      EQU %00000000
+DEF ENT_FLAG_TYPE           EQU %01111000
 DEF ENT_TYPE_PLAYER         EQU %00000000
-DEF ENT_TYPE_CHEST          EQU %00100000
-DEF ENT_TYPE_ENEMY          EQU %01000000
-DEF ENT_TYPE_BOMB           EQU %01100000
-DEF ENT_FLAG_INITIALIZED    EQU %00010000
-DEF ENT_INITIALIZED         EQU %00010000
-DEF ENT_UNINITIALIZED       EQU %00000000
-
+DEF ENT_TYPE_CHEST          EQU %00001000
+DEF ENT_TYPE_ENEMY          EQU %00010000
+DEF ENT_TYPE_BOMB           EQU %00011000
+; Offsets de cada atributo
 DEF ENT_FLAGS               EQU 0
 DEF ENT_MOMENTUM_MAX        EQU 1
 DEF ENT_MOMENTUM_X          EQU 2
@@ -43,10 +39,8 @@ DEF ENT_MOMENTUM_Y          EQU 3
 DEF ENT_MOMENTUM_INC_DEC    EQU 4
 DEF ENT_HEALTH              EQU 5
 DEF ENT_DAMAGE              EQU 6
-; Codificado en Little Endian
-DEF ENT_METADATA_PTR        EQU 7
-DEF ENT_SCALED_X            EQU 9
-DEF ENT_SCALED_Y            EQU 11
+DEF ENT_SCALED_X            EQU 7
+DEF ENT_SCALED_Y            EQU 9
 
 SECTION "Entity List", WRAM0
 ; Número de huecos libres en la lista de entidades.
@@ -71,9 +65,9 @@ EntityManager_func_initialize::
 
 ; Inicializa una entidad.
 ;
-; create_entity(hl = entity_metadata* data, a = ENT_TYPE) returns none;
+; create_entity(a = ENT_TYPE) returns none;
 ;
-; Destruye: a, b.
+; Destruye: a, b, hl.
 EntityManager_func_create_entity::
     ld b, a
     ; Comprobar si quedan entradas libres
@@ -86,33 +80,18 @@ EntityManager_func_create_entity::
     ld [EntityListRemaining], a
 
     ; Inicializar la nueva entrada.
-    ld a, ENT_VALID
+    ld a, ENT_STATUS_VALID
     or b ; ENT_TYPE
-    or ENT_UNINITIALIZED
     ld [ptr_next_free + ENT_FLAGS], a
-
-    ld a, LOW(HL)
-    ld [ptr_next_free + ENT_METADATA_PTR], a
-    ld a, HIGH(HL)
-    ld [ptr_next_free + 1 + ENT_METADATA_PTR], a
 
     ; Obtener la siguiente entrada libre, si es que quedan huecos
     ld a, [EntityListRemaining]
     cp 0
     ret z
 
-    push hl
-
     ; Buscar de manera circular en la lista
-    .searchLoop:
-    ld a, l
-    add ENT_SZ
-    ld l, a
-    ld a, h
-    adc 0
-    ld h, a
-
-
+.searchLoop:
+    call get_next
     ; HL =< EntityListEnd implica
     ; H < HIGH(EntityListEnd) OR (H = HIGH(EntityListEnd) AND L =< LOW(EntityListEnd) )
 
@@ -125,15 +104,15 @@ EntityManager_func_create_entity::
     cp l
     jr nc, .isLess
 
-    .isGreather:
+.isGreather:
     ld hl, EntityListStart
 
-    .isLess:
+.isLess:
     ; Comprobar si el hueco está disponible  
     ld a, [hl + ENT_FLAGS]
-    and ENT_FLAG_VALID
-    cp ENT_INVALID
-    jr nz, .searchLoop
+    and ENT_FLAG_STATUS
+    cp ENT_STATUS_VALID
+    jr z, .searchLoop
 
     ; Guardar el hueco libre como el siguiente a utilizar
     ld a, l
@@ -165,18 +144,15 @@ EntityManager_func_destroy_entity::
 ;
 ; clear_all() returns none;
 ;
-; Destruye: a, b, hl, de
+; Destruye: a, b, hl
 EntityManager_func_clear_all::
 
     ld b, ENT_LIST_MAX 
     ld hl, EntityListStart 
-    xor a
-    ld d, a 
-    ld e, ENT_SZ
 
-    .loop:
-    ld [hl], a
-    add hl, de 
+.loop:
+    ld [hl], ENT_STATUS_INVALID
+    call get_next
 
     dec b 
     cp b 
@@ -190,8 +166,49 @@ EntityManager_func_clear_all::
 ; update() returns none;
 ;
 EntityManager_func_update::
+    ld hl, EntityListStart
+    ld b, ENT_LIST_MAX
 
+    ; Actualizar la lógica del jugador, que siempre es la primera entidad en la lista
+    
+.loop:
+    call get_next
+
+    dec b
+    xor a
+    cp b
+    jr nc, .end:
+    
+    ld a, [hl + ENT_FLAGS]
+    and ENT_FLAG_STATUS
+    cp ENT_STATUS_VALID
+    jr nz, .loop
+
+    ; Comprobar el tiempo de la entidad.
+    ; Saltar a la dirección de memoria de comienzo de la actualización de su lógica.
+    
+    ld a, [hl + ENT_FLAGS]
+    and ENT_FLAG_TYPE
+
+    cp, ENT_TYPE_BOMB
+    jr nz, .checkEnemy
+    ; Lógica de la bomba
+    jr .loop
+
+.checkEnemy:
+    cp, ENT_TYPE_ENEMY
+    jr nz, .checkChest
+    ; Lógica del enemigo
+    jr .loop
+
+.checkChest
+    cp, ENT_TYPE_CHEST
+    ; Lógica del cofre
+    jr nz, .loop
+
+.end:
     ret
+
 
 ; Obtiene el comienzo del siguiente elemento de la lista
 ;
@@ -199,24 +216,11 @@ EntityManager_func_update::
 ;
 ; Destruye; a, hl.
 get_next:
-    ld a, ENT_SZ ; 2c 2B
-    add l ; 1 1
-    ld l, a; 1 1
-    ld a, h ; 1 1
-    adc 0 ; 2 2
-    ld h, l ; 1 1
+    ld a, ENT_SZ
+    add l
+    ld l, a
+    ld a, h
+    adc 0 
+    ld h, l
     
-    ; Total 8c 8B
-    
-    push de ; 4 1
-    ld e, ENT_SZ ; 2 2
-    ld d, 0 ; 2 2
-
-    add hl, de ; 2 1
-
-    pop de ; 3 1
-    ; Total 13c 7B
-
     ret
-
-
